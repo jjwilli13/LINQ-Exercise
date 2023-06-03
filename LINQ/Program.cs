96f9e8b4d27e3dc7de71910b20ed044ac917fc59
Jordan Williams <jjwilli13@gmail.com>
2023-06-03 06:01:10 -0500

    completed exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace LINQ
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            List<string> myGames = new List<string>() { "NFL Blitz 2000", "Spiderman", "MLB The Show", "Tomb Raider", "Donkey Kong" };
+
+            IEnumerable<string> games = myGames.OrderBy(myGames => myGames.Length);
+
+
+
+            foreach (var game in games)
+            {
+                Console.WriteLine(game);
+            }
+            
+
+
+
         }
     }
 }
